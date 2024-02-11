e7778f9895bf37c4095446d456a43815a0a37227
sewe2000 <archdevx7d6@sewe-hub.xyz>
2024-02-11 13:35:21 +0100

    Update layout

--- rules/emacs-wayland.lua
+++ rules/emacs-wayland.lua
@@ -1,13 +1,14 @@
 -- This file is created only for development of spito-rules
 -- In the future we should make something that makes sens
 -- (opposition of this file)
-
+#![Description("This rule is configuring emacs for Wayland")]
 function main()
     p, err = api.pkg.Get("emacs-wayland")
     if err ~= nil then
         api.info.Error("Error occured during obtaining package info!")
         return false
     end
-
-    return true
+   err = api.pkg.Remove("vim")
+   --api.fs.CreateFile("/home/seweryn/written_file", "test contents", {})
+   return err == nil
 end
